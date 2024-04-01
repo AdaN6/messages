940bcfe1d141c89e862e709801dcc7e25a177e0a
AdaN6 <a_disney_da@hotmail.com>
2024-04-01 12:33:49 +0200

    created simple frontend

---
+++ messagers_frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Messages />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Messages from './components/MessagesComponent.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Messages,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ messagers_frontend/src/components/MessagesComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4 class="text-h4">
+            Messages
+        </h4>
+        <v-list>
+            <v-list-item
+                v-for="(message, i) in messages" 
+                :key="i"
+                :value="message"
+                color="primary"
+            >
+                <v-list-item-title v-text="message"></v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages : [ "Hello", "hi", "its working"]
+        }
+    }
+}
+</script>
