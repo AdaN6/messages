6f1042b87bb4befa77878921f2dd87a4980f7cc4
AdaN6 <a_disney_da@hotmail.com>
2024-04-01 17:20:33 +0200

    typo

--- messages_backend/server.js
+++ messages_backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 // app.get('/', (req,res) => res.send('test'))
 app.get('/messages', (req,res) => {
-    const messages = [ "yes", "tes", "is working"];
+    const messages = [ "yes", "test", "is working"];
 
     res.send(messages);
 })
