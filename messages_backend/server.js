3b161e1694e2ca978f47a29d891e89496349e0e0
AdaN6 <a_disney_da@hotmail.com>
2024-04-02 14:11:13 +0200

    saving data to a list

--- messages_backend/server.js
+++ messages_backend/server.js
@@ -8,17 +8,21 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json())
 
+const messages = [ "yes", "test", "is working"];
+
 // app.get('/', (req,res) => res.send('test'))
 app.get('/messages', (req,res) => {
-    const messages = [ "yes", "test", "is working"];
+    // const messages = [ "yes", "test", "is working"];
 
     res.send(messages);
 })
 
 app.post('/messages', (req,res) => {
-    console.log(req.body);
-
-    res.json({})
+    let msg = req.body
+    console.log(msg);
+    messages.push(msg.message);
+    res.json(msg);
+    console.log(messages);
 })
 
 app.listen(port, () => console.log('app running'))
