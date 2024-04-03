31240dbc4f32d227bb91e479164643801bf33fe8
AdaN6 <a_disney_da@hotmail.com>
2024-04-03 17:03:20 +0200

    created params api

--- messages_backend/server.js
+++ messages_backend/server.js
@@ -17,6 +17,11 @@ app.get('/messages', (req,res) => {
     res.send(messages);
 })
 
+app.get("/messages/:id", (req, res) => {
+  // console.log(req.params.id)
+  res.send(messages[req.params.id]);
+});
+
 app.post('/messages', (req,res) => {
     let msg = req.body
     console.log(msg);
